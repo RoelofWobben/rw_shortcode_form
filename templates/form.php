68878ef3d9e949c0db21eec8aa4403ec3fc14bed
RoelofWobbenfrontend <104582123+RoelofWobbenfrontend@users.noreply.github.com>
2024-02-17 21:57:22 +0100

    some changes requested

---
+++ templates/form.php
@@ -0,0 +1,27 @@
+<form method="post" action="" >
+    <div id="response_div">
+
+    </div>
+    <div class="ideapro_form">
+        <label for="your_name">Your Name</label>
+        <input type="text" name="your_name" id="your_name" placeholder="Your Full Name" />
+
+        <label for="your_email">Your Email Address</label>
+        <input type="email" name="your_email" id="your_email" placeholder="Enter Your Email Address" />
+
+        <label for="phone_number">Phone Number</label>
+        <input type="text" name="phone_number" id="phone_number" placeholder="Your Phone Number" />
+
+        <label for="your_comments">Questions or Comments</label>
+        <textarea name="your_comments" id="your_comments" placeholder="Say something nice"></textarea>
+
+        <br />
+        <br />
+        
+        <input type="submit" name="ideapro_contact_submit" id="ideapro_contact_submit" onclick="submit_contact_form()" value="SUBMIT YOUR INFORMATION" />';
+
+    </div>
+
+</form>
+
+
