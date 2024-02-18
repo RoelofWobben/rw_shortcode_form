86be90fd26b3255609c1d24978fdcc873dd22026
RoelofWobbenfrontend <104582123+RoelofWobbenfrontend@users.noreply.github.com>
2024-02-18 19:30:03 +0100

    changes spaces with tabs

--- mycustomForm.php
+++ mycustomForm.php
@@ -1,26 +1,30 @@
-<?php 
-/**
-* Plugin Name: Idea Pro Contact Form Ajax
-* Description: This form submits through an ajax call
-**/
+<?php
 
-function roelof_add_custom_shortcode() {
-	function roelof_contact_form($atts, $content, $shortcode_tag) {
-	  ob_start();
-	  load_template(__DIR__ . '/templates/form.php', false, [
-		'atts' => $atts,
-		'content' => $content,
-		'shortcode_tag' => $shortcode_tag,
-	  ]);
-	  return ob_end_flush();
+/**
+ * Plugin Name: Idea Pro Contact Form Ajax
+ * Description: This form submits through an ajax call
+ **/
+
+function roelof_add_custom_shortcode()
+{
+	function roelof_contact_form($atts, $content, $shortcode_tag)
+	{
+		ob_start();
+		load_template(__DIR__ . '/templates/form.php', false, [
+			'atts' => $atts,
+			'content' => $content,
+			'shortcode_tag' => $shortcode_tag,
+		]);
+		return ob_end_flush();
 	}
-  
+
 	add_shortcode('contact_form', 'roelof_contact_form');
-  }
+}
 
 add_action('init', 'roelof_add_custom_shortcode');
 
-function load_assets() {
+function load_assets()
+{
 	wp_enqueue_style(
 		'mycustomForm',
 		plugin_dir_url(__FILE__) . '/css/mycustomForm.css',
@@ -28,8 +32,6 @@ function load_assets() {
 		1,
 		'all'
 	);
-
 }
 
 add_action('wp_enqueue_scripts', 'load_assets');
-
