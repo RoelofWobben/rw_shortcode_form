23661290e299a623f31fe1edda2a53b3361ee4ce
RoelofWobbenfrontend <104582123+RoelofWobbenfrontend@users.noreply.github.com>
2024-02-18 19:13:29 +0100

    added a domain to the right functions

--- templates/form.php
+++ templates/form.php
@@ -8,18 +8,18 @@
         <?php wp_nonce_field('submit_contact_form', 'contact_form_nonce'); ?>
         <div class="form-group">
             <input id="<?php echo $shortcode ?>_input" type="text" required value="<?php echo esc_attr($atts['subject']) ?> " />
-            <label for="<?php echo $shortcode ?>_input"  class="control-label"><?php esc_html_e("Subject") ?></label>
+            <label for="<?php echo $shortcode ?>_input"  class="control-label"><?php esc_html_e("Subject", "mycustomForm") ?></label>
         </div>
         <div class="form-group">
             <input id="<?php echo $shortcode ?>_email"type="text" required value="<?php echo esc_attr($atts['email']) ?> "/>
-            <label for="<?php echo $shortcode ?>_email" class="control-label" ><?php esc_html_e("Email") ?></label>
+            <label for="<?php echo $shortcode ?>_email" class="control-label" ><?php esc_html_e("Email", "mycustomForm") ?></label>
         </div>
         <div class="form-group">
             <textarea id="<?php echo $shortcode ?>_message"required value="<?php echo esc_attr($atts['message']) ?>"> </textarea>
-            <label for="<?php echo $shortcode ?>_message" class="control-label"><?php esc_html_e("Message") ?></label>
+            <label for="<?php echo $shortcode ?>_message" class="control-label"><?php esc_html_e("Message", "mycustomForm") ?></label>
         </div>
         <div class="button-container">
-            <button class="button"><span>Submit</span></button><?php esc_html_e("Subject") ?>
+            <button class="button"><span>Submit</span></button><?php esc_html_e("Subject", "mycustomForm") ?>
         </div>
 
     </form>
