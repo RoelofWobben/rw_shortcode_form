194962cb01c3e81928e2629cdc653a3952ff2a20
RoelofWobbenfrontend <104582123+RoelofWobbenfrontend@users.noreply.github.com>
2024-02-18 17:27:43 +0100

    esc the input properly

--- templates/form.php
+++ templates/form.php
@@ -1,19 +1,19 @@
-<?php  $atts = array_replace(["subject" =>  "", "email" => "" , "message" => ""], $args['atts']); ?>
+<?php  $atts = array_replace(["class" => "" , "subject" =>  "", "email" => "" , "message" => ""], $args['atts']); ?>
 
-<div class="<?php echo $atts['class'] ?>" data-shortcode="<?php echo $args['shortcode_tag'] ?>">
+<div class="<?php echo esc_attr($atts['class']) ?>" data-shortcode="<?php echo esc_attr($args['shortcode_tag']) ?>">
             <form action="#" method="post">
                 <h1>Contact Form</h1>
                 <?php wp_nonce_field( 'submit_contact_form', 'contact_form_nonce' ); ?>
                 <div class="form-group">
-                    <input type="text" required />
+                    <input type="text" required value="<?php echo esc_attr($atts['subject']) ?> " />
                     <label for="input" class="control-label">Subject</label>
                 </div>
                 <div class="form-group">
                     <input type="text" required />
-                    <label for="input" class="control-label">Email</label>
+                    <label for="input" class="control-label" value="<?php echo esc_attr($atts['email']) ?> " >Email</label>
                 </div>
                 <div class="form-group">
-                    <textarea required></textarea>
+                    <textarea required value="<?php echo esc_attr($atts['messsage']) ?> " ></textarea>
                     <label for="textarea" class="control-label">Message</label>
                 </div>
                 <div class="button-container">
