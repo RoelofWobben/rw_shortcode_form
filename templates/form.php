d807227967b0a9d950c0f5ab11b0c56412164942
RoelofWobbenfrontend <104582123+RoelofWobbenfrontend@users.noreply.github.com>
2024-02-18 19:27:51 +0100

    fix indenditation and replace php echo with =

--- templates/form.php
+++ templates/form.php
@@ -3,24 +3,24 @@
    $shortcode =  esc_attr($args['shortcode_tag']) 
 ?> 
 
-<div class="<?php echo esc_attr($atts['class']) ?>" data-shortcode="<?php echo esc_attr($args['shortcode_tag']) ?>">
+<div class="<?= esc_attr($atts['class']) ?>" data-shortcode="<?= esc_attr($args['shortcode_tag']) ?>">
     <form action="#" method="post">
         <?php wp_nonce_field('submit_contact_form', 'contact_form_nonce'); ?>
         <div class="form-group">
-            <input id="<?php echo $shortcode ?>_input" type="text" required value="<?php echo esc_attr($atts['subject']) ?> " />
-            <label for="<?php echo $shortcode ?>_input"  class="control-label"><?php esc_html_e("Subject", "mycustomForm") ?></label>
+            <input id="<?= $shortcode ?>_input" type="text" required value="<?= esc_attr($atts['subject']) ?> " />
+            <label for="<?= $shortcode ?>_input"  class="control-label"><?php esc_html_e("Subject", "mycustomForm") ?></label>
         </div>
         <div class="form-group">
-            <input id="<?php echo $shortcode ?>_email"type="text" required value="<?php echo esc_attr($atts['email']) ?> "/>
-            <label for="<?php echo $shortcode ?>_email" class="control-label" ><?php esc_html_e("Email", "mycustomForm") ?></label>
+            <input id="<?= $shortcode ?>_email"type="text" required value="<?= esc_attr($atts['email']) ?> "/>
+            <label for="<?= $shortcode ?>_email" class="control-label" ><?php esc_html_e("Email", "mycustomForm") ?></label>
         </div>
         <div class="form-group">
-            <textarea id="<?php echo $shortcode ?>_message"required value="<?php echo esc_attr($atts['message']) ?>"> </textarea>
-            <label for="<?php echo $shortcode ?>_message" class="control-label"><?php esc_html_e("Message", "mycustomForm") ?></label>
+            <textarea id="<?= $shortcode ?>_message"required value="<?= esc_attr($atts['message']) ?>"> </textarea>
+            <label for="<?= $shortcode ?>_message" class="control-label"><?php esc_html_e("Message", "mycustomForm") ?></label>
         </div>
         <div class="button-container">
             <button class="button"><span><?php esc_html_e("Submit", "mycustomForm") ?></span></button>
         </div>
 
     </form>
-</div>
+</div> 
