700115fbe30cce0f63fde94745612a8d17f3187c
RoelofWobbenfrontend <104582123+RoelofWobbenfrontend@users.noreply.github.com>
2024-02-26 20:31:37 +0100

    still trying to find out why the forms returning nothing

--- submit.php
+++ submit.php
@@ -34,14 +34,20 @@ if ($errors->has_errors()) {
     wp_send_json_error($errors);
 }
 
+ob_start(); 
+load_template(__DIR__ . '/templates/email.php', false, [
+   ' data' => $data
+]); 
+$template = ob_get_clean(); 
+
 $headers = array('Content-Type: text/html; charset=UTF-8');
-$mail_send = wp_mail(get_option( 'admin_email' ), $data['email'],load_template(__DIR__ . '/templates/email.php', false, [
+$mail_send = wp_mail(get_option( 'admin_email' ), $data['email'],$template, false, [
     'data' => $data
- ]), $headers);
+ ], $headers);
 
 
 if (!$mail_send) {
     wp_send_json(new WP_Error("Error", "Mail cannot be send")); 
 }
 
-wp_send_json(new WP_Error(200, "Mail is send")); 
+
