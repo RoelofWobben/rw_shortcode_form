fdf852ae696773913f1d7e867ad3e21b9bd6480e
RoelofWobbenfrontend <104582123+RoelofWobbenfrontend@users.noreply.github.com>
2024-03-04 21:43:02 +0100

    some refractorings

--- js/myCustomForm.js
+++ js/myCustomForm.js
@@ -1,9 +1,44 @@
+/**
+ * @typedef {Object} ServerError
+ * @property {String} code
+ * @property {String} message
+ * 
+ * @typedef {Object} ServerResponse
+ * @property {Boolean} success
+ * @property {?Array.<ServerError>} data
+ */
+
+
+
+/**
+ * @type NodeListOf<HTMLFormElement>
+ */
 var all_forms = document.querySelectorAll('[data-shortcode="contact_form"] > form');
 
 /**
-* Function to send form data to the backend
+* Call to the backend
 * @param {FormData} data - The form data
 * @param {string} url - The backend URL
+* @return {Promise<ServerResponse>} 
+*/
+async function send_to_backend(data, url) {
+	var response = await fetch(url, {
+		method: 'POST',
+		body: data
+	})
+	return response.json();
+}
+
+
+
+
+var all_forms = document.querySelectorAll('[data-shortcode="contact_form"] > form');
+
+/**
+* Call to backend
+* @param {FormData} data - The form data
+* @param {string} url - The backend URL
+* @return {Promise | Object} 
 */
 async function send_to_backend(data, url) {
 	var response = await fetch(url, {
@@ -22,14 +57,12 @@ all_forms.forEach((form) => {
 	/**
 	*  @type {HTMLDivElement}
 	*/
-
 	var userFeedbackDiv = document.querySelector('.user_feedback');
 
 	/**
 	* Function to reset user feedback
 	* @return {void}
 	*/
-
 	var resetUserFeedback = function resetUserFeedback() {
 		userFeedbackDiv.classList.add('hidden');
 		while (userFeedbackDiv.firstChild) {
@@ -124,10 +157,8 @@ all_forms.forEach((form) => {
 		}
 
 		/**
-		* Call to the backend 
-		*  @param {FormData} formdata- The form data
-		*  @param {string} form.action - the action of the form 
-		*/
+		 * @type {ServerResponse}
+		 */
 		var backend_response = await send_to_backend(formdata, form.action);
 
 		if (backend_response['success']) {
